6324ade05a399f7e9f4f0368de23651933625deb
1 <koreanka937@gmail.com>
2022-03-03 22:53:28 +0500

    завершение

--- gulp/tasks/html.js
+++
@@ -1,5 +0,0 @@
-export const html = () => {
-	return app.gulp.src(app.path.src.html)
-		.pipe(app.gulp.dest(app.path.build.html))
-		.pipe(app.plugins.browserSync.stream());
-}
--- src/script/burger.js
+++
@@ -1,7 +0,0 @@
-const burger = document.querySelector('.burger');
-const navigation = document.querySelector('.navigation');
-
-burger.addEventListener('click', () => {
-	burger.classList.toggle('burger_active');
-	navigation.classList.toggle('navigation_active');
-});
